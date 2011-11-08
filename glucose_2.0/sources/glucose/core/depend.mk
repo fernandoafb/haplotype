312fb3c43167fcfda5c8504db2143461d52fcd99
fernando.afb@gmail.com <fernando.afb@gmail.com@e9904f9c-7382-e204-0859-7f492fdfc051>
2011-11-08 15:49:01 +0000

    First commit

---
+++ glucose_2.0/sources/glucose/core/depend.mk
@@ -0,0 +1,32 @@
+/home/fernando/Downloads/glucose_2.0/sources/glucose/core/Main.o /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Main.or /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Main.od /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Main.op: /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Main.cc \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../utils/System.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/IntTypes.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../utils/ParseUtils.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../utils/Options.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Vec.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/XAlloc.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/Dimacs.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/SolverTypes.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Alg.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Map.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Alloc.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/Solver.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Heap.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/BoundedQueue.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/Constants.h
+/home/fernando/Downloads/glucose_2.0/sources/glucose/core/Solver.o /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Solver.or /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Solver.od /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Solver.op: \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/Solver.cc \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Sort.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Vec.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/IntTypes.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/XAlloc.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/Solver.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Heap.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Alg.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../utils/Options.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../utils/ParseUtils.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/SolverTypes.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Map.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../mtl/Alloc.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/BoundedQueue.h \
+ /home/fernando/Downloads/glucose_2.0/sources/glucose/core/../core/Constants.h
---
+++ src/com/fbraz/haplotype/src/GeneSequence.java
@@ -0,0 +1,101 @@
+package com.fbraz.haplotype.src;
+
+import java.util.ArrayList;
+
+public class GeneSequence {
+	
+	public int numSites;
+	
+	public ArrayList<Integer> genes;
+	public ArrayList<Integer> vars;
+	public ArrayList<Literal> values;
+
+	public ArrayList<Integer> gA;
+	public ArrayList<Integer> gB;
+	
+	public ArrayList<Integer> selectorSetA;
+	public ArrayList<Integer> selectorSetB;
+	
+	public ArrayList<Integer> additionalA;
+	public ArrayList<Integer> additionalB;	
+	
+	private Integer consideratedHaplotypes;
+	
+	public GeneSequence(String seq, Integer v) {
+		numSites = seq.length();
+		if (numSites > 0) {
+			genes = new ArrayList<Integer>();
+			vars = new ArrayList<Integer>();
+			gA = new ArrayList<Integer>();
+			gB = new ArrayList<Integer>();
+			values = new ArrayList<Literal>();
+			for (int i = 0; i < seq.length(); i++) {
+				try{
+					genes.add(Integer.valueOf(""+seq.charAt(i)));
+				}
+				catch(NumberFormatException nfex) {
+					genes.add(2);
+				}
+				vars.add(v++);
+				gA.add(v++);
+				gB.add(v++);
+			} 
+		}
+	}
+	
+	public GeneSequence(Integer m, Integer v) {
+		numSites = m;
+		if (numSites > 0) {
+			genes = new ArrayList<Integer>();
+			vars = new ArrayList<Integer>();
+			gA = new ArrayList<Integer>();
+			gB = new ArrayList<Integer>();	
+			values = new ArrayList<Literal>();
+			for (int i = 0; i < m; i++) {
+				vars.add(v++);
+			} 
+		}
+	}	
+	
+	public void selectorVariable(Integer c, Integer v) {
+		consideratedHaplotypes = c;
+		selectorSetA = new ArrayList<Integer>();
+		selectorSetB = new ArrayList<Integer>();
+		for (int i = 0; i < consideratedHaplotypes; i++)
+			selectorSetA.add(v++);
+		for (int j = 0; j < consideratedHaplotypes; j++)
+			selectorSetB.add(v++);			
+	}
+	
+	public void additionalVariables(Integer v) {
+		additionalA = new ArrayList<Integer>();
+		additionalB = new ArrayList<Integer>();
+		for (int i = 0; i < consideratedHaplotypes; i++)
+			additionalA.add(v++);
+		for (int j = 0; j < consideratedHaplotypes; j++)
+			additionalB.add(v++);			
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for (Integer gene : genes)
+			sb.append(gene);
+		return sb.toString();
+	}
+	
+	public String printLiterais() {
+		StringBuffer sb = new StringBuffer();
+		for (Literal lit : values)
+			sb.append(lit+" ");
+		return sb.substring(0,sb.length() > 0 ? sb.length()-1 : 0).toString();
+	}
+	
+	public String printValues() {
+		StringBuffer sb = new StringBuffer();
+		for (Literal lit : values)
+			sb.append(lit.getBinary());
+		return sb.toString();
+	}
+	
+	
+}
